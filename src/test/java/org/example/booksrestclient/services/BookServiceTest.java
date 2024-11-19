efd79481b6b154ea5853d0bf080b37a424418376
BohdanBezdrabko <bozbogdan05@ukr.net>
2024-11-19 22:48:07 +0100

    Add unit tests

---
+++ src/test/java/org/example/booksrestclient/services/BookServiceTest.java
@@ -0,0 +1,26 @@
+package org.example.booksrestclient.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class BookServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void deleteBookById() {
+    }
+}
